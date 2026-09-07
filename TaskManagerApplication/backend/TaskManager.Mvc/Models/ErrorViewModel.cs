2b8e2ede0cbd1f67c4d4c94cd3fb50196b4b8d80
Manoj-left <manoj.m01@aveva.com>
2026-09-07 22:50:49 +0530

    removed razor views and related files

--- TaskManagerApplication/backend/TaskManager.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TaskManager.Mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
