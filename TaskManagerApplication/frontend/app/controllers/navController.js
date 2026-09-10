bb3712f8f004d690e7c9f4208cdcb535a3bf68a2
Manoj-left <manoj.m01@aveva.com>
2026-09-10 15:36:38 +0530

    fixed nav bar

--- TaskManagerApplication/frontend/app/controllers/navController.js
+++ TaskManagerApplication/frontend/app/controllers/navController.js
@@ -10,6 +10,12 @@ taskManagerApp.controller('NavController', ['$rootScope', '$location', 'authServ
     refresh();
     $rootScope.$on('$routeChangeSuccess', refresh);
 
+    // startsWith so /tasks/:id/edit still marks the Tasks link active
+    nav.isActive = function (path) {
+        var current = $location.path();
+        return current === path || current.indexOf(path + '/') === 0;
+    };
+
     nav.logout = function () {
         authService.logout();
         refresh();
